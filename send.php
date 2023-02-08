fd6bb67dd99b927c5eb314751ba91e0ef429a926
D1gout <fanolkon@gmail.com>
2023-02-09 01:30:32 +0500

    New elements

---
+++ send.php
@@ -0,0 +1,18 @@
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+
+$fio = urldecode($fio);
+$email = urldecode($email);
+
+$fio = trim($fio);
+$email = trim($email);
+
+if (mail("fanolkon@gmail.com", "Заказ с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: fanolkon@icloud.com \r\n"))
+ { 
+    echo "сообщение успешно отправлено"; 
+} else { 
+    echo "при отправке сообщения возникли ошибки"; 
+}
